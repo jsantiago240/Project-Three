4eb0d3f39bcc8fa397ccd1d1d9c6f8d7f214ec5b
Javius Santiago <jvsjgs123@gmail.com>
2023-10-20 08:07:28 -0400

    Initial commit

---
+++ Headers/ItemsSold.h
@@ -0,0 +1,17 @@
+#ifndef HEADERS_ITEMSSOLD_H
+#define HEADERS_ITEMSSOLD_H
+
+#include <string>
+#include <map>
+
+using namespace std;
+
+// ItemsSold Class, map containing each item sold and its quantity
+class ItemsSold {
+    public:
+    private:
+        map<string, int> itemMap;
+
+};
+
+#endif
